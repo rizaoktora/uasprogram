cc477250f32aa14cebde9af16bd3961e4526a74f
unknown <rizaoktora>
2023-11-21 20:30:31 +0700

    Commit Pertama

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+  
+@section('title', 'Selamat Datang di Riza-Store')
+  
+@section('contents')
+  <div class="row">
+    Silahkan melihat katalog  dan kami akan melayani Anda dengan senang hati !
+  </div>
+@endsection
